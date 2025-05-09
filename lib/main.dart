7cefe746dfbb90f44332d1fa7f930e232327d843
Kyoya Iwatsuru <sd24098u@st.omu.ac.jp>
2025-05-09 11:16:29 +0900

    feat: todoタイル作成

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ,
+      home: TodoListScreen(),
     );
   }
 }
 
+class TodoListScreen extends StatelessWidget {
+  const TodoListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo App'),),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              radius: 4,
+              backgroundColor: Colors.green,
+            ),
+            title: const Text('sample todo task'),
+            subtitle: const Text('18:00'),
+            trailing: Checkbox(
+              value: false,
+              onChanged: (value) {}
+            ),
+          )
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
