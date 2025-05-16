5b4060f55e3821862278471cb1fbbb929db07e3f
Kyoya Iwatsuru <sd24098u@st.omu.ac.jp>
2025-05-16 16:14:48 +0900

    feature: ボタン

--- lib/screens/add_todo/add_todo_screen.dart
+++ lib/screens/add_todo/add_todo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study_2025/screens/add_todo/components/add_todo_bottom_buttons.dart';
 import 'package:flutter_study_2025/screens/add_todo/components/add_todo_categories.dart';
 import 'package:flutter_study_2025/screens/add_todo/components/add_todo_inputs.dart';
 import 'package:flutter_study_2025/screens/add_todo/components/add_todo_schedule.dart';
@@ -9,17 +10,22 @@ class AddTodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create New Task'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: const [
-          AddTodoInputs(),
-          SizedBox(height: 16),
-          AddTodoCategories(),
-          SizedBox(height: 16),
-          AddTodoSchedule(),
+      appBar: AppBar(title: const Text('Create New Task')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              children: const [
+                AddTodoInputs(),
+                SizedBox(height: 16),
+                AddTodoCategories(),
+                SizedBox(height: 16),
+                AddTodoSchedule(),
+              ],
+            ),
+          ),
+          const AddTodoBottomButtons(),
         ],
       ),
     );
